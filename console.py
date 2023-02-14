8ac5188560016a3fb04f355006e728d3699baf2f
Ngece <ngecemlamli@gmail.com>
2023-02-14 06:56:59 -0800

    committing another done

--- console.py
+++ console.py
@@ -28,6 +28,28 @@ class HBNBCommand(cmd.cmd):
         "Review"
     }
 
+    match = re.search(r"\.", arg)
+    if match is not None:
+        argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+        match = re.search(r"\((.*?)\)", argl[1])
+        if match is not None:
+            command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+            if command[0] in argdict.keys():
+                call = "{} {}".format(argl[0], command[1])
+                return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.all,
+            "show": self.show,
+            "destroy": self.destroy,
+            "count": self.count,
+            "update": self.update
+        }
+
     def quit(self, arg):
         """Quit command to exit the program."""
         return True
@@ -150,7 +172,7 @@ class HBNBCommand(cmd.cmd):
                     obj.__dict__[k] = v
         storage.save()
 
-    def do_count(self, arg):
+    def count(self, arg):
         """ Thisi will retrieve the number of instances of a given class."""
         argl = parse(arg)
         count = 0
@@ -158,6 +180,23 @@ class HBNBCommand(cmd.cmd):
             if argl[0] == obj.__class__.__name__:
                 count += 1
         print(count)
+
+    def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 if __name__ == "__main__":
